f92d21cdbc3a6a64e03e2630d2e68993da15fbad
njacooper <neal.cooper@me.com>
2022-11-15 19:52:52 +0000

    Added socket.io and cors to server.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,26 @@
 const express = require('express')
-const app = express()
+const app = require('express')()
+const server = require('http').createServer(app)
 const port = 8080
+const cors = require('cors')
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+})
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Video Chat')
 })
 
-app.listen(port, () => {
+io.on('connection', socket => {
+  console.log('socket id: ', socket.id)
+})
+
+server.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
