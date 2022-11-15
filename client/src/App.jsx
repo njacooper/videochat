9f0f6637445e9605e5eb31050b2fce10ffb0dc82
njacooper <neal.cooper@me.com>
2022-11-15 19:52:04 +0000

    Added socket client to app.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,17 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
+import { io } from 'socket.io-client'
+
+const socket = io('http://localhost:8080')
 
 function App() {
+  
+  // Test connection to socket server
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('socket.id: ', socket.id)
+    })
+  }, [])
+
   return (
     <main>
       <h1>Video Chat</h1>
